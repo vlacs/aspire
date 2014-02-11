6891934791e9584f8c4103caf16d792c26359104
Tom Dolan <tgdolan@gmail.com>
2014-02-10 20:15:54 -0500

    Adding ultra-basic datomic query test. A real test would make a mock request of a url that, in turn, hit the datomic db.

--- test/aspire/web_test.clj
+++ test/aspire/web_test.clj
@@ -2,7 +2,9 @@
   (:require [aspire.web :as a-web]
             [clojure.test :refer :all]
             [ring.mock.request :as rmr]
-            [aspire.web-test-config :refer :all]))
+            [aspire.web-test-config :refer :all]
+            [datomic.api :refer [db q] :as d]
+            [aspire.db-config :as a-db]))
 
 ;; web-test-config makes the mock ring requests to the resources
 ;; defined in web.clj. The resulting vars are available for testing here.
@@ -18,5 +20,11 @@
  
 ;; Disabling this temporarily; see my comments in web_test_config. --moquist
 #_(deftest resource-app-test
-  (is (= (:status onboarding-response) 200)))
+    (is (= (:status onboarding-response) 200)))
+
+;; sample datomic acess test 
+(deftest sample-datomic-test
+  ;; sample test query from datomic getting started
+  (def results (d/q '[:find ?c :where [?c :community/name]] (d/db a-db/conn)))
+  (is (> (count results) 0 )))
 
