ac32dc830d56d36fb545988efa87c178dd457f43
Matt Oquist <moquist@majen.net>
2014-02-18 13:52:49 -0500

    rename project

--- dev-set-up.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-myconfsdir=~/.aspire
-
-mkdir -p "$myconfsdir"
-cp -vi sample-confs/* "$myconfsdir"
--- test/aspire/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns aspire.core-test
-  (:require [clojure.test :refer :all]
-            [aspire.core :refer :all]))
-
-(deftest pass-test
-  (testing "I pass"
-    (is (= 1 1))))
